90aa908d23ea0b061ecb867891283c13145e83c1
idisfkj <idisfkj@gmail.com>
2020-09-04 10:03:57 +0800

    feat: Linked => 链表中环的检测

---
+++ kotlin/src/com/daily/algothrim/linked/algo/Circle.kt
@@ -0,0 +1,45 @@
+package com.daily.algothrim.linked.algo
+
+import com.daily.algothrim.linked.LinkedNode
+
+/**
+ * 链表中环的检测
+ * 使用快慢指针，慢指针每次走一步，快指针每次走两步；当快指针等于慢指针时，说明快指针已经领先慢指针一圈，即该链表存在环。
+ */
+class Circle {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val head = LinkedNode(1)
+            println(Circle().existCircle(head.apply {
+                next = LinkedNode(2).apply {
+                    next = LinkedNode(3).apply {
+                        next = LinkedNode(2).apply {
+                            next = LinkedNode(1).apply {
+                                next = head
+                            }
+                        }
+                    }
+                }
+            }))
+        }
+    }
+
+    /**
+     * O(n)
+     */
+    fun existCircle(linkedList: LinkedNode<Int>?): Boolean {
+
+        var low = linkedList
+        var fast = linkedList
+
+        while (fast?.next != null) {
+            low = low?.next
+            fast = fast.next?.next
+            if (fast == low) return true
+        }
+
+        return false
+    }
+}
