12b462689e9fbc64b28f3d1f3601d4a60236f690
idisfkj <idisfkj@gmail.com>
2020-09-26 10:14:46 +0800

    feat: lookup => 二分查找最后一个值等于给定的值

--- kotlin/src/com/daily/algothrim/lookup/BinarySearchV2.kt
+++ kotlin/src/com/daily/algothrim/lookup/BinarySearchV2.kt
@@ -1,15 +1,15 @@
 package com.daily.algothrim.lookup
 
 /**
- * 二分查找第一个等于给定的值
+ * 二分法查找最后一个值等于给定的值
  */
-class BinarySearchV1 {
+class BinarySearchV2 {
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             val a = intArrayOf(1, 2, 4, 5, 6, 9, 29, 93, 93, 93, 95, 213, 741)
-            println(BinarySearchV1().search(a, a.size, 93))
+            println(BinarySearchV2().search(a, a.size, 93))
         }
     }
 
@@ -19,6 +19,7 @@ class BinarySearchV1 {
     fun search(a: IntArray, n: Int, value: Int): Int {
         var low = 0
         var high = n - 1
+
         while (low <= high) {
             val mid = low + (high - low).shr(1)
             when {
@@ -29,8 +30,8 @@ class BinarySearchV1 {
                     high = mid - 1
                 }
                 else -> {
-                    if (mid == 0 || a[mid - 1] != value) return mid
-                    high = mid - 1
+                    if (mid == n - 1 || a[mid + 1] != value) return mid
+                    low = mid + 1
                 }
             }
         }
