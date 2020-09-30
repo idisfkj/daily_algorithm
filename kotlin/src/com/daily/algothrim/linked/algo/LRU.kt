98e2327105182a2e4b143279864c8ce477c118b0
idisfkj <idisfkj@gmail.com>
2020-09-30 10:13:12 +0800

    fix: 将LUR修改成LRU

--- kotlin/src/com/daily/algothrim/linked/algo/LRU.kt
+++ kotlin/src/com/daily/algothrim/linked/algo/LRU.kt
@@ -3,20 +3,20 @@ package com.daily.algothrim.linked.algo
 import com.daily.algothrim.linked.LinkedNode
 
 /**
- * 使用单链表实现LUR
+ * 使用单链表实现LRU
  * 1. 如果此数据之前已经被缓存在链表中了，我们遍历得到这个数据对应的结点，并将其从原来的位置删除，然后再插入到链表的头部。
  * 2. 如果此数据没有在缓存链表中，又可以分为两种情况：
  * 2.1 如果此时缓存未满，则将此结点直接插入到链表的头部;
  * 2.2 如果此时缓存已满，则链表尾结点删除，将新的数据结点插入链表的头部。
  */
-class LUR {
+class LRU {
 
     companion object {
         private const val MAX_SIZE = 5
 
         @JvmStatic
         fun main(args: Array<String>) {
-            LUR().apply {
+            LRU().apply {
                 cache(null, 1).apply {
                     cache(this, 2).apply {
                         cache(this, 3).apply {
