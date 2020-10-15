d03c91e58666e0bf20abf9a10ce6d46460f9bace
idisfkj <idisfkj@gmail.com>
2020-10-15 13:57:40 +0800

    fix: 修改二叉树中序遍历的逻辑

--- kotlin/src/com/daily/algothrim/tree/TreeNode.kt
+++ kotlin/src/com/daily/algothrim/tree/TreeNode.kt
@@ -2,8 +2,8 @@ package com.daily.algothrim.tree
 
 data class TreeNode<T>(var data: T, var left: TreeNode<T>? = null, var right: TreeNode<T>? = null) {
     fun printMid(node: TreeNode<T>) {
-        println(node.data)
         node.left?.let { printMid(it) }
+        println(node.data)
         node.right?.let { printMid(it) }
     }
 
