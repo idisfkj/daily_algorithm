4d366e1fd6f924c0a9d62416b1f9b30068ce5b88
idisfkj <idisfkj@gmail.com>
2021-04-07 23:31:41 +0800

    fix: 添加说明

--- kotlin/src/com/daily/algothrim/leetcode/RemoveDuplicates.kt
+++ kotlin/src/com/daily/algothrim/leetcode/RemoveDuplicates.kt
@@ -2,6 +2,9 @@ package com.daily.algothrim.leetcode
 
 /**
  * 80. 删除有序数组中的重复项 II
+ *
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 最多出现两次 ，返回删除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
  */
 class RemoveDuplicates {
 
@@ -40,6 +43,8 @@ class RemoveDuplicates {
         }
     }
 
+    // 输入：nums = [1,1,1,2,2,3]
+    // 输出：5, nums = [1,1,2,2,3]
     fun removeDuplicates(nums: IntArray): Int {
         val size = nums.size
         if (size <= 2) return size
