4ced10a05daa742f24a96657a74a01b803df6fbb
idisfkj <idisfkj@gmail.com>
2024-03-13 10:23:17 +0800

    feat: leetcode => 串联所有单词的子窜(30)

---
+++ kotlin/src/com/daily/algothrim/leetcode/top150/FindSubstring.kt
@@ -0,0 +1,111 @@
+package com.daily.algothrim.leetcode.top150
+
+/**
+ * 30. 串联所有单词的子串
+ */
+
+/*
+给定一个字符串 s 和一个字符串数组 words。 words 中所有字符串 长度相同。
+
+ s 中的 串联子串 是指一个包含  words 中所有字符串以任意顺序排列连接起来的子串。
+
+例如，如果 words = ["ab","cd","ef"]， 那么 "abcdef"， "abefcd"，"cdabef"， "cdefab"，"efabcd"， 和 "efcdab" 都是串联子串。 "acdbef" 不是串联子串，因为他不是任何 words 排列的连接。
+返回所有串联子串在 s 中的开始索引。你可以以 任意顺序 返回答案。
+
+
+
+示例 1：
+
+输入：s = "barfoothefoobarman", words = ["foo","bar"]
+输出：[0,9]
+解释：因为 words.length == 2 同时 words[i].length == 3，连接的子字符串的长度必须为 6。
+子串 "barfoo" 开始位置是 0。它是 words 中以 ["bar","foo"] 顺序排列的连接。
+子串 "foobar" 开始位置是 9。它是 words 中以 ["foo","bar"] 顺序排列的连接。
+输出顺序无关紧要。返回 [9,0] 也是可以的。
+示例 2：
+
+输入：s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+输出：[]
+解释：因为 words.length == 4 并且 words[i].length == 4，所以串联子串的长度必须为 16。
+s 中没有子串长度为 16 并且等于 words 的任何顺序排列的连接。
+所以我们返回一个空数组。
+示例 3：
+
+输入：s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+输出：[6,9,12]
+解释：因为 words.length == 3 并且 words[i].length == 3，所以串联子串的长度必须为 9。
+子串 "foobarthe" 开始位置是 6。它是 words 中以 ["foo","bar","the"] 顺序排列的连接。
+子串 "barthefoo" 开始位置是 9。它是 words 中以 ["bar","the","foo"] 顺序排列的连接。
+子串 "thefoobar" 开始位置是 12。它是 words 中以 ["the","foo","bar"] 顺序排列的连接。
+
+
+提示：
+
+1 <= s.length <= 104
+1 <= words.length <= 5000
+1 <= words[i].length <= 30
+words[i] 和 s 由小写英文字母组成
+ */
+class FindSubstring {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            FindSubstring().findSubstring("barfoothefoobarman", arrayOf("foo", "bar")).forEach {
+                print("$it,")
+            }
+            println()
+            FindSubstring().findSubstring("wordgoodgoodgoodbestword", arrayOf("word", "good", "best", "word")).forEach {
+                print("$it,")
+            }
+            println()
+            FindSubstring().findSubstring("barfoofoobarthefoobarman", arrayOf("bar", "foo", "the")).forEach {
+                print("$it,")
+            }
+            println()
+            FindSubstring().findSubstring("wordgoodgoodgoodbestword", arrayOf("word", "good", "best", "good")).forEach {
+                print("$it,")
+            }
+            println()
+            FindSubstring().findSubstring("bcabbcaabbccacacbabccacaababcbb", arrayOf("c","b","a","c","a","a","a","b","c")).forEach {
+                print("$it,")
+            }
+        }
+    }
+
+    fun findSubstring(s: String, words: Array<String>): List<Int> {
+        val map = hashMapOf<String, Int>()
+        words.forEach {
+            map[it] = map.getOrDefault(it, 0) + 1
+        }
+        val wordsSize = words[0].length
+        val length = words.size * wordsSize
+        val result = arrayListOf<Int>()
+        if (s.length < length) return result
+        var index: Int
+        for (i in 0..s.length - length) {
+            index = i
+            while (index < i + length) {
+                val curr = s.substring(index, index + wordsSize)
+                if (map.getOrDefault(curr, -1) <= 0) {
+                    if (index > i) {
+                        map.clear()
+                        words.forEach {
+                            map[it] = map.getOrDefault(it, 0) + 1
+                        }
+                    }
+                    break
+                }
+                index += wordsSize
+                map[curr] = map.getOrDefault(curr, 0) - 1
+            }
+            if (index == i + length) {
+                result.add(i)
+                words.forEach {
+                    map[it] = map.getOrDefault(it, 0) + 1
+                }
+            }
+        }
+        return result
+    }
+}
